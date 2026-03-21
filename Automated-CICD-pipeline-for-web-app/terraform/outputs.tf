0e91c145b888b725b591a8506e9c1d53aa2711ae
Garv  Changrani <154041471+Garvity@users.noreply.github.com>
2026-03-21 18:40:24 +0530

    Delete Automated-CICD-pipeline-for-web-app directory

--- Automated-CICD-pipeline-for-web-app/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ec2_public_ip" {
-  description = "Public IP of EC2"
-  value       = aws_instance.flask.public_ip
-}
-
-output "ec2_public_dns" {
-  description = "Public DNS of EC2"
-  value       = aws_instance.flask.public_dns
-}
